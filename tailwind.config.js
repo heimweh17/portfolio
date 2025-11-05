5ebc5655ee2175ae6ddd031cd5216ed5d89eed98
heimweh17 <71220635+heimweh17@users.noreply.github.com>
2025-11-05 03:15:07 -0500

    Update Tailwind config to include JSX files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
-﻿ /** @type {import('tailwindcss').Config} */
- export default {
-   content: ["./index.html", "./src/**/*.{ts,tsx}"],
-   theme: { extend: {} },
-   plugins: [],
- };
+
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", 
+  ],
+  theme: { extend: {} },
+  plugins: [],
+}
