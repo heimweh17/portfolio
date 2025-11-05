b78b2839e1a964d7f14b6acfd0d50f50d5a0bbbe
heimweh17 <71220635+heimweh17@users.noreply.github.com>
2025-11-05 18:23:53 -0500

    Add routing with BrowserRouter and Routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import "./index.css";
 import App from './App.tsx'
+import PhotoGallery from './PhotoGallery.tsx'
+import PortfolioChinese from './PortfolioChinese.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/photos" element={<PhotoGallery />} />
+        <Route path="/zh" element={<PortfolioChinese />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
