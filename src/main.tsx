aa08d777591d07a19a95b1a66dbb623b4d96d11c
heimweh17 <71220635+heimweh17@users.noreply.github.com>
2025-12-20 12:34:11 -0500

    blog

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import "./index.css";
 import App from './App.tsx'
 import PhotoGallery from './PhotoGallery.tsx'
 import PortfolioChinese from './PortfolioChinese.tsx'
-
+import Blog from './Blog.tsx'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
@@ -13,6 +13,7 @@ createRoot(document.getElementById('root')!).render(
         <Route path="/" element={<App />} />
         <Route path="/photos" element={<PhotoGallery />} />
         <Route path="/zh" element={<PortfolioChinese />} />
+        <Route path="/blog" element={<Blog />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>,
